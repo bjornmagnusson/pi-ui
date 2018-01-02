5bd1cf9ab085513f0bc250199566e7a09a8000c0
Björn Magnusson <bjornmagnusson1984@gmail.com>
2018-01-02 18:37:38 +0100

    Added initial node express api

--- app.js
+++ app.js
@@ -2,18 +2,34 @@ const express = require('express')
 const app = express()
 const unirest = require('unirest')
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.get('/gpios', function(req, res) {
-  unirest.post('http://192.168.0.110:8080/v1/gpios')
-  .end(function (response) {
-    console.log(response.body);
-    return response.body;
+const PORT = process.env.PORT || 3000
+const API_HOST = process.env.API_HOST || "pi1.local"
+const API_PORT = process.env.API_PORT || 8080
+const API_URL = "http://" + API_HOST + ":" + API_PORT
+
+console.log("API: " + API_URL)
+
+const GPIOS_URL = API_URL + "/v1/gpios"
+app.get('/gpios', function (req, res) {
+  console.log('Fetch GPIOs');
+  unirest.get(GPIOS_URL).end(function (response) {
+    console.log(response)
+    res.send(response.body)
   });
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+const LEDMODE_URL = API_URL + "/v1/ledMode"
+app.post('/ledMode', function(req, res) {
+  console.log('Toggle LED mode')
+  unirest.post(LEDMODE_URL).end(function (response) {
+    res.send({status: "OK"})
+  })
+})
+
+app.listen(PORT, function () {
+  console.log('Example app listening on port ' + PORT)
 })
